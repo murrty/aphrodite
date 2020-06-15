6f7c0d38a479a4d8b56aae9d00d8e237fd2f9915
matty <10895809+murrty@users.noreply.github.com>
2020-06-15 16:23:44 -0500

    2.0 release comit
    stuff. i moved ookii dialog stuff to a separate folder for maintenance, but there's duplicates in the repo.

--- aphrodite/Forms/frmLicensing.Designer.cs
+++ aphrodite/Forms/frmLicensing.Designer.cs
@@ -33,7 +33,8 @@
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(521, 261);
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(522, 261);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
@@ -41,10 +42,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 261);
+            this.ClientSize = new System.Drawing.Size(522, 261);
             this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(538, 300);
             this.Name = "frmLicensing";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Licensing";
             this.ResumeLayout(false);
 
