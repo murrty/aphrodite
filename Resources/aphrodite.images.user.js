c3f76109b71ab4e028be8b6217e2aef92ca3160a
matty <10895809+murrty@users.noreply.github.com>
2020-06-19 20:57:01 -0500

    Update images userscript for new ui

--- Resources/aphrodite.images.user.js
+++ Resources/aphrodite.images.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        aphrodite images
 // @namespace   https://github.com/murrty/aphrodite
-// @version     1.2
+// @version     1.3
 // @description e621 image downloader
 // @run-at      document-end
-// @include     http*://e621.net/post/show/*
-// @include     http*://*.e621.net/post/show/*
+// @include     http*://e621.net/posts/*
+// @include     http*://*.e621.net/posts/*
 // @downloadURL https://github.com/murrty/aphrodite/raw/master/Resources/aphrodite.images.user.js
 // @updateURL   https://github.com/murrty/aphrodite/raw/master/Resources/aphrodite.images.user.js
 // @grant       none
@@ -15,7 +15,9 @@
 var imageDiv = document.createElement('div');
 var imageLink = document.createElement('a');
 var imageSettings = document.createElement('a');
-var fImagesSidebar = document.getElementsByClassName('post-sidebar-header')[0];
+var fImagesSidebar = document.getElementById('blacklist-box');
+var imageSpacer = document.createElement('br');
+var imageSpacerTwo = document.createElement('br');
 
 imageDiv.id = "paginator";
 
@@ -24,13 +26,15 @@ imageLink.title = "Download this image with aphrodite";
 imageLink.href = "images:" + document.URL;
 imageLink.appendChild(document.createTextNode('download image'));
 imageDiv.appendChild(imageLink);
+imageDiv.appendChild(imageSpacer);
 
 imageSettings.id = "image-settings";
 imageSettings.href = "images:configuresettings";
 imageSettings.title = "Change the Image Downloader settings";
-imageSettings.appendChild(document.createTextNode('settings'));
+imageSettings.appendChild(document.createTextNode('image settings'));
 imageDiv.appendChild(imageSettings);
+imageDiv.appendChild(imageSpacerTwo);
 
-imageDiv.style = "display: normal; text-align: left; padding: 1em 18px 1em;";
+//imageDiv.style = "display: normal; text-align: left; padding: 1em 18px 1em;";
 imageDiv.className = "status-notice";
 fImagesSidebar.parentNode.insertBefore(imageDiv, fImagesSidebar);
