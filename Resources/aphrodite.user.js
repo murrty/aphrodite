7b56fe1543574b9e0f1b78ddc7624039ace82292
matty <10895809+murrty@users.noreply.github.com>
2020-06-19 20:48:12 -0500

    Update userscript to suit new ui

--- Resources/aphrodite.user.js
+++ Resources/aphrodite.user.js
@@ -1,69 +1,26 @@
 // ==UserScript==
 // @name        Aphrodite
 // @namespace   https://github.com/murrty/aphrodite
-// @version     1.4
+// @version     1.5
 // @description e621 general downloader (pools & images based on tags)
 // @run-at      document-end
-// @include     http*://e621.net/pool/show/*
-// @include     http*://*.e621.net/pool/show/*
-// @include     http*://e621.net/post/show/*
-// @include     http*://*.e621.net/post/show/*
-// @include     http*://*.e621.net/post/index/*/*
-// @include     http*://e621.net/post/index/*/*
-// @include     http*://*.e621.net/post?tags=*
-// @include     http*://e621.net/post?tags=*
+// @include     http*://e621.net/pools/*
+// @include     http*://*.e621.net/pools/*
+// @include     http*://e621.net/posts*
+// @include     http*://*.e621.net/posts*
 // @downloadURL https://github.com/murrty/aphrodite/raw/master/Resources/aphrodite.user.js
 // @updateURL   https://github.com/murrty/aphrodite/raw/master/Resources/aphrodite.user.js
 // @grant       none
 // ==/UserScript==
 
-// Tag
-if (document.URL.indexOf("e621.net/post/index/") > -1 || document.URL.indexOf("e621.net/post?tags=") > -1) {
-    var tagDiv = document.createElement('div');
-    var tagDownloadLink = document.createElement('a');
-    var tagDownloadTags = document.createElement('a');
-    var tagSettings = document.createElement('a');
-    var tagElement = document.getElementById('blacklisted-sidebar');
-    var tagSpacer = document.createElement('br');
-    var tagSpacerTwo = document.createElement('br');
-    var tagSpacerThree = document.createElement('br');
-    var tagSpacerFour = document.createElement('br');
-
-    tagDiv.id = "paginator";
-    tagDiv.style = "display: normal; text-align: left; padding: 2px 2px 1em;"
-    tagDiv.className = "pagination";
-
-    tagDownloadLink.id = "download-page";
-    tagDownloadLink.href = "tags:" + document.URL;
-    tagDownloadLink.title = "Download this page using aphrodite";
-    tagDownloadLink.appendChild(document.createTextNode('download this page'));
-    tagDiv.appendChild(tagDownloadLink);
-    tagDiv.appendChild(tagSpacer);
-    tagDiv.appendChild(tagSpacerTwo);
-
-    tagDownloadTags.id = "download-tag";
-    tagDownloadTags.href = "tags:" + document.getElementsByName('tags')[0].value.replace(" ", " tags:");
-    tagDownloadTags.title = "Download searched tag(s) using aphrodite";
-    tagDownloadTags.appendChild(document.createTextNode('download searched tag(s)'));
-    tagDiv.appendChild(tagDownloadTags);
-    tagDiv.appendChild(tagSpacerThree);
-    tagDiv.appendChild(tagSpacerFour);
-
-    tagSettings.id = "download-settings";
-    tagSettings.href = "tags:configuresettings";
-    tagSettings.title = "Change aphrodite's tag downloading settings";
-    tagSettings.appendChild(document.createTextNode('change downloader settings'));
-    tagDiv.appendChild(tagSettings);
-    tagElement.parentNode.insertBefore(tagDiv, tagElement);
-}
 
 // Pool
-if (document.URL.indexOf("e621.net/post/show/") > -1 || document.URL.indexOf("e621.net/pool/show/") > -1) {
+if (document.URL.indexOf("e621.net/posts/") > -1 || document.URL.indexOf("e621.net/pools/") > -1) {
     var poolDiv = document.createElement('div');
     var poolLink = document.createElement('a');
     var poolWish = document.createElement('a');
     var poolSettings = document.createElement('a');
-    var fPoolElement = document.getElementById('del-mode');
+    var fPoolElement = document.getElementById('blacklist-box');
     var fPoolSidebar = document.getElementsByClassName('post-sidebar-header')[0];
     var poolSpacer = document.createElement('br');
 
@@ -81,7 +38,7 @@ if (document.URL.indexOf("e621.net/post/show/") > -1 || document.URL.indexOf("e6
 
     poolDiv.id = "paginator";
 
-    if (document.URL.indexOf("e621.net/pool/show/") > -1) {
+    if (document.URL.indexOf("e621.net/pools/") > -1) {
         poolDiv.appendChild(poolSpacer);
     }
 
@@ -100,15 +57,15 @@ if (document.URL.indexOf("e621.net/post/show/") > -1 || document.URL.indexOf("e6
     poolSettings.appendChild(document.createTextNode('settings'));
     poolDiv.appendChild(poolSettings);
 
-    if (document.URL.indexOf("e621.net/post/show/") > -1) {
-        poolDiv.style = "display: normal; text-align: left; padding: 1em 23px 1em;"
-        poolDiv.className = "status-notice";
-        poolLink.href = "pools:https://e621.net/pool/show/" + poolID;
-        if (tag.id.lastIndexOf("pool", 0) === 0) {
-            fPoolSidebar.parentNode.insertBefore(poolDiv, fPoolSidebar);
-        }
-    }
-    else if (document.URL.indexOf("e621.net/pool/show/") > -1) {
+    //if (document.URL.indexOf("e621.net/posts/") > -1) {
+    //    poolDiv.style = "display: normal; text-align: left; padding: 1em 23px 1em;"
+    //    poolDiv.className = "status-notice";
+    //    poolLink.href = "pools:https://e621.net/pool/show/" + poolID;
+    //    if (tag.id.lastIndexOf("pool", 0) === 0) {
+    //        fPoolSidebar.parentNode.insertBefore(poolDiv, fPoolSidebar);
+    //    }
+    //}
+    if (document.URL.indexOf("e621.net/pools/") > -1) {
         poolDiv.style = "display: normal; text-align: left; padding: 0px 0px 0px;"
         poolDiv.className = "pageination";
         poolLink.href = "pools:" + document.URL;
@@ -117,3 +74,40 @@ if (document.URL.indexOf("e621.net/post/show/") > -1 || document.URL.indexOf("e6
         fPoolElement.parentNode.insertBefore(poolDiv, fPoolElement);
     }
 }
+// Tag
+else if (document.URL.indexOf("e621.net/posts") > -1 || document.URL.indexOf("e621.net/posts?tags=") > -1) {
+    var tagDiv = document.createElement('div');
+    var tagDownloadLink = document.createElement('a');
+    var tagDownloadTags = document.createElement('a');
+    var tagSettings = document.createElement('a');
+    var tagElement = document.getElementById('blacklist-box');
+    var tagSpacer = document.createElement('br');
+    var tagSpacerTwo = document.createElement('br');
+
+    tagDiv.id = "paginator";
+    tagDiv.style = "display: normal; text-align: left; padding: 2px 2px 1em;"
+    tagDiv.className = "pagination";
+
+    tagDownloadLink.id = "download-page";
+    tagDownloadLink.href = "tags:" + document.URL;
+    tagDownloadLink.title = "Download this page using aphrodite";
+    tagDownloadLink.appendChild(document.createTextNode('download this page'));
+    tagDiv.appendChild(tagDownloadLink);
+    tagDiv.appendChild(tagSpacer);
+
+    tagDownloadTags.id = "download-tag";
+    var foundTags = document.getElementsByName('tags')[0].value;
+    tagDownloadTags.href = "tags:" + foundTags.replace(" ", " tags:");
+    tagDownloadTags.title = "Download searched tag(s) using aphrodite";
+    tagDownloadTags.appendChild(document.createTextNode('download searched tag(s)'));
+    tagDiv.appendChild(tagDownloadTags);
+    tagDiv.appendChild(tagSpacerTwo);
+
+    tagSettings.id = "download-settings";
+    tagSettings.href = "tags:configuresettings";
+    tagSettings.title = "Change aphrodite's tag downloading settings";
+    tagSettings.appendChild(document.createTextNode('change downloader settings'));
+    tagDiv.appendChild(tagSettings);
+    tagElement.parentNode.insertBefore(tagDiv, tagElement);
+}
+
